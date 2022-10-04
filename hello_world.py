1fa59e62abd2497544028c9fb713281d99eb6a7d
muuuuulo <114501138+123456789kat78@users.noreply.github.com>
2022-10-04 16:39:22 +0000

    лабы

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,1 @@
-print('Hello world!') 
-print(' колобок повесился')
+print
---
+++ lec_1_input.py
@@ -0,0 +1,10 @@
+a = input()
+print(a)
+
+a = input('введение значени a: ')
+print(a)
+print(type(a))
+
+a = int(input('Введение значение a: '))
+print(a)
+print(type(a))
