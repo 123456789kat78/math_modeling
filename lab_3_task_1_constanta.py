e7389d45c47c236b2231b4aec24f196987a371ce
muuuuulo <114501138+123456789kat78@users.noreply.github.com>
2022-11-15 14:52:53 +0000

    15.11.22

---
+++ lab_3_task_1_constanta.py
@@ -0,0 +1,6 @@
+import numpy 
+gravitacionnaya_postoyannaya = 6,67 * 10 ** (-11)
+skorost_sveta = 3 * 10 ** 8
+elektron_mass = 1,6 * 10 * (-19)
+atomnaya_edinica_mass = 1.6 *10 **(-19)
+postoyannaya_planka = 6,63 * 10 **(-34)
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
     deps = [
+        pkgs.python38Packages.pip
         pkgs.python39Packages.pip
         pkgs.python39Full
         pkgs.cowsay
