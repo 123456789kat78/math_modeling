ad3a5ef50ff8b4c6964d0b46f43c6cb74a37dba0
muuuuulo <114501138+123456789kat78@users.noreply.github.com>
2022-12-06 14:54:37 +0000

    06.12 4 лаба

---
+++ lab_4_task_5.py
@@ -0,0 +1,10 @@
+def area(*arg< **kw):
+    if kw['figure'] == 'square':
+        s = arg[0] ** 2
+    elif kw['figure'] == 'rectangle':
+        s = arg[0] * arg[1]
+    else:
+        s = 1 / 2 * arg[0] * arg[1]
+        print(s)
+
+area(2 , 5, 7,figure = 'triangle')
---
+++ lab_4_task_dop_1.py
@@ -0,0 +1,7 @@
+def power(a ,n):
+    p = 1
+    for i in range(n):
+        p = p * a
+    print(p)
+
+power(3, 2)
